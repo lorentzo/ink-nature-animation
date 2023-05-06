710ae3bef9087680028cb218e0d5446a6d9d163b
Lovro Bosnar <lovro.bosnar1@gmail.com>
2023-05-06 10:25:21 +0200

    refactor, remove redundant, create functions, call on collections, create multiple splines per polygon

--- procedural.py
+++ procedural.py
@@ -3,10 +3,9 @@ import bpy
 import mathutils
 import bmesh
 
-def deselect_all():
-    for obj in bpy.data.objects:
-        obj.select_set(False)
-    bpy.context.view_layer.objects.active = None 
+# Enhance freestyle
+# store in collections
+# draw style on faces, not only edges.
 
 def select_activate_only(objects=[]):
     for obj in bpy.data.objects:
@@ -16,25 +15,12 @@ def select_activate_only(objects=[]):
         obj.select_set(True)
         bpy.context.view_layer.objects.active = obj
 
-def create_bezier_curve(n_cuts):
-    deselect_all()
-    bpy.ops.curve.primitive_bezier_curve_add(radius=1, enter_editmode=True, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
-    if n_cuts > 0:
-        bpy.ops.curve.subdivide(number_cuts=n_cuts)
-    bpy.ops.object.editmode_toggle()
-    curve_obj = bpy.context.selected_objects[0]
-    curve_obj.data.bevel_depth = 0.01
-    return curve_obj
-
-def create_nurbs_curve(n_cuts):
-    deselect_all()
-    bpy.ops.curve.primitive_nurbs_path_add(radius=1, enter_editmode=True, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
-    if n_cuts > 0:
-        bpy.ops.curve.subdivide(number_cuts=n_cuts)
-    bpy.ops.object.editmode_toggle()
-    created_curve = bpy.context.selected_objects[0]
-    created_curve.data.bevel_depth = 0.01
-    return created_curve
+def copy_obj(obj):
+    obj_cpy = obj.copy()
+    obj_cpy.data = obj.data.copy()
+    obj_cpy.animation_data_clear()
+    bpy.context.collection.objects.link(obj_cpy)
+    return obj_cpy
 
 def perturb_curve(curve_obj, perturb_scale=1.0, perturb_strength=1.0, n_octaves=1, amplitude_scale=1.0, frequency_scale=1.0):
     curve_type = curve_obj.data.splines[0].type
@@ -46,7 +32,7 @@ def perturb_curve(curve_obj, perturb_scale=1.0, perturb_strength=1.0, n_octaves=
     for point in points:
         point_co = mathutils.Vector((point.co[0], point.co[1], point.co[2]))       
         trans_vec = mathutils.noise.turbulence_vector(
-            point_co * perturb_scale, 
+            point_co * perturb_scale * mathutils.noise.random(), 
             n_octaves,
             False, #hard
             noise_basis='PERLIN_ORIGINAL',
@@ -56,17 +42,27 @@ def perturb_curve(curve_obj, perturb_scale=1.0, perturb_strength=1.0, n_octaves=
         if curve_type == "BEZIER":
             point.co = (new_point_co[0], new_point_co[1], new_point_co[2])
         if curve_type == "POLY" or curve_type == "NURBS":
-            point.co = (new_point_co[0], new_point_co[1], new_point_co[2], point.co[3])
+            point.co = (new_point_co[0], new_point_co[1], new_point_co[2], point.co[3]) # Note: https://blender.stackexchange.com/questions/220812/what-is-the-4th-coordinate-of-spline-points
     return curve_obj
 
+def convert_mesh_to_curve(mesh_obj, curve_bevel_depth=0.01, curve_n_subdiv=0):
+    select_activate_only([mesh_obj])
+    bpy.ops.object.convert(target='CURVE') # creates POLY by default
+    curve = bpy.context.selected_objects[0]
+    curve.data.bevel_depth = curve_bevel_depth
+    bpy.ops.object.editmode_toggle()
+    if curve_n_subdiv > 0:
+        bpy.ops.curve.subdivide(number_cuts=curve_n_subdiv)
+    bpy.ops.object.editmode_toggle()
+    return curve
 
-# Get cube on which splines will be created
-base_obj = bpy.context.selected_objects[0]
-base_obj_vertices = base_obj.data.vertices
-
-# For each base_obj face create spline
-for polygon in base_obj.data.polygons:
-    # For each polygon create mesh containing edges.
+def create_edge_mesh_from_polygon(base_obj: bpy.types.Object, polygon: bpy.types.MeshPolygon):
+    """
+        Take mesh object and its polygon, create mesh containing only polygon edges.
+        Polygon: https://docs.blender.org/api/current/bpy.types.MeshPolygon.html#bpy.types.MeshPolygon
+    """
+    # Fetch info on base object.
+    base_obj_vertices = base_obj.data.vertices
     bm = bmesh.new()
     for poly_edge_key in polygon.edge_keys:
         bmv1 = bm.verts.new(base_obj_vertices[poly_edge_key[0]].co)
@@ -74,23 +70,33 @@ for polygon in base_obj.data.polygons:
         bm.edges.new((bmv1, bmv2))
     bmesh.ops.remove_doubles(bm, verts=bm.verts, dist=1e-4)
     mesh = bpy.data.meshes.new("mesh")
-    obj = bpy.data.objects.new("obj", mesh)
-    bpy.context.collection.objects.link(obj)
+    obj_mesh = bpy.data.objects.new("obj", mesh)
+    bpy.context.collection.objects.link(obj_mesh)
     bm.to_mesh(mesh)
     bm.free()
-    # Convert created mesh to curve, subdivide, displace.
-    #for i in range(int(mathutils.noise.random()*10)):
-    select_activate_only([obj])
-    bpy.ops.object.convert(target='CURVE')
-    curve = bpy.context.selected_objects[0]
-    curve.data.bevel_depth = 0.01
-    bpy.ops.object.editmode_toggle()
-    bpy.ops.curve.subdivide(number_cuts=10)
-    bpy.ops.object.editmode_toggle()
-    perturb_curve(curve, perturb_scale=mathutils.noise.random()*5.0, perturb_strength=0.2, n_octaves=1, amplitude_scale=0.3, frequency_scale=1.0)
+    return obj_mesh
 
+def main():
 
+    # Apply on all objects in collection.
+    for base_obj in bpy.data.collections['bobj'].all_objects:
+        # For each base object face create spline.
+        for polygon in base_obj.data.polygons:
+            # For each polygon create mesh containing the same edges.
+            polygon_edges_mesh_obj = create_edge_mesh_from_polygon(base_obj, polygon)
+            # For each created polygon edges, create curve.
+            for i in range(3):
+                edges_cpy = copy_obj(polygon_edges_mesh_obj)
+                # Convert created mesh to curve.
+                curve = convert_mesh_to_curve(edges_cpy, curve_bevel_depth=0.01, curve_n_subdiv=10) # TODO: randomize
+                # Pertub curve.
+                perturb_curve(curve, perturb_scale=5.0, perturb_strength=0.2, n_octaves=1, amplitude_scale=0.3, frequency_scale=1.0)
 
+#
+# Script entry point.
+#
+if __name__ == "__main__":
+    main()
 
 
 
